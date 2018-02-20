004a14c76f91c18183ec8507bcc3e7a2d3d6f8a0
jwillis44 <jwillis44@gmail.com>
2018-02-20 18:24:49 -0500

    Update fixsearch.sh
    Commented and removed specific domain information.

--- fixsearch.sh
+++ fixsearch.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
-dscl /Search -delete / CSPSearchPath "/Active Directory/CORP/All Domains"
-dscl /Search/Contacts -delete / CSPSearchPath "/Active Directory/CORP/corp.m-ecs.org"
-dscl /Search -append / CSPSearchPath "/Active Directory/CORP/corp.m-ecs.org"
+# Changes macOS Search Path from /All Domains to FQDN allowing for correct opperation of MCX
+#
+# Var's need data !!
+# NBSN - Netbios Short name
+# NBSN=corp
+NBSN=
+#
+#FQDN=corp.contoso.com
+FQDN=
+#
+#
+# Delete Current Search Path
+dscl /Search -delete / CSPSearchPath "/Active Directory/$NBSN/All Domains"
+dscl /Search/Contacts -delete / CSPSearchPath "/Active Directory/$NBSN/$FQDN"
+# Add Correct Search Path
+dscl /Search -append / CSPSearchPath "/Active Directory/$NBSN/$FQDN"
 sleep 10
-dscl /Search/Contacts -append / CSPSearchPath "/Active Directory/CORP/corp.m-ecs.org"
+# Add Contacts search path
+dscl /Search/Contacts -append / CSPSearchPath "/Active Directory/$NBSN/$FQDN"
 sleep 10
-
+exit
+#end of script
